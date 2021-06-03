c7aabacd764c72532b628196bc7e3caaa600df99
Fer <fernafleck@gmail.com>
2021-06-03 12:44:58 -0300

    tabla de datos de potencia instalada

--- frontend/src/components/Potencia.js
+++ frontend/src/components/Potencia.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Bar } from 'react-chartjs-2';
 //import PotenciaInstalada from './potenciaInstalada.json';
 import axios from 'axios';
+import Tabla from './Tabla.js';
 
 function Potencia(props) {
 
@@ -119,19 +120,19 @@ function Potencia(props) {
       {
         label: 'Potencia Hidráulica',
         data: potenciasHidraulica,
-        backgroundColor: 'rgb(240, 246, 110, 0.7)',
+        backgroundColor: 'rgb(168, 198, 134, 0.7)',
         stack:1,
       },
       {
         label: 'Potencia Biomasa',
         data: potenciasBiomasa,
-        backgroundColor: 'rgb(168, 198, 134, 0.7)',
+        backgroundColor: 'rgb(157, 156, 98, 0.7)',
         stack:1,
       },
       {
         label: 'Potencia Solar',
         data: potenciasSolar,
-        backgroundColor: 'rgb(157, 156, 98, 0.7)',
+        backgroundColor: 'rgb(240, 246, 110, 0.7)',
         stack:1,
       },
     ],
@@ -139,14 +140,24 @@ function Potencia(props) {
   
   const options = {
     scales: {
-      yAxes: [
-        {
-          stacked: true,
+      y: {
+        stacked: true,
           ticks: {
             beginAtZero: true,
           },
-        },
-      ],
+        display: true,
+        title: { 
+          display: true,
+          text: 'Potencia (MW)',
+          color: 'black',
+          font: {
+            size: 20,
+            style: 'normal',
+            lineHeight: 2
+          },
+          padding: {top: 0, left: 0, right: 0, bottom: 0}
+        }
+      },
       xAxes: [
         {
           stacked: true,
@@ -157,10 +168,14 @@ function Potencia(props) {
 
   return(
     <>
-      <div className='header'>
+      <div className='header mt-4 mb-2'>
         <h1 className='title'>Potencia Instalada</h1>
       </div>
       <Bar data={data} options={options} />
+      <div className='header mt-4 mb-2'>
+        <h2 className='title'>Datos</h2>
+      </div>
+      <Tabla potencias={potenciaInstalada} />
     </>
   );
 }
---
+++ frontend/src/components/Tabla.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function Tabla(props) {
+  return (
+    <div className="w-100 table-responsive vh-100 px-3 mb-3 overflow-auto">
+        <table className="table table-striped table-sm align-middle">
+            <thead className="position-sticky top-0 table-dark">
+                <tr>
+                    <th scope="col" className="text-uppercase text-center align-middle">Año</th>
+                    <th scope="col" className="text-uppercase text-center align-middle">Central</th>
+                    <th scope="col" className="text-uppercase text-center align-middle">Agente</th>
+                    <th scope="col" className="text-uppercase text-center align-middle">Fuente de Generación</th>
+                    <th scope="col" className="text-uppercase text-center align-middle">Tecnología</th>
+                    <th scope="col" className="text-uppercase text-center align-middle">Potencia (MW)</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.potencias.map(p => {
+                    return(
+                        <tr className="">
+                            <th scope="row" className="text-center">{p.Año}</th>
+                            <td className="text-uppercase fst-italic">{p.central}</td>
+                            <td className="text-uppercase fst-italic">{p.agente_descripcion}</td>
+                            <td className="text-uppercase fst-italic">{p.fuente_generacion}</td>
+                            <td className="text-uppercase fst-italic">{p.tecnologia}</td>
+                            <th scope="row" className="text-end">{p.MW}</th>
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
+    </div>
+  );
+}
+
+export default Tabla;
